ccca707725408fba064970ab4193c5416ba98369
AngeloFrancisco97 <49500499+AngeloFrancisco97@users.noreply.github.com>
2021-07-07 20:24:23 -0300

    Subindo Projeto

---
+++ precache-manifest.1f4b5edb032a4c8353488635e3f5d865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf5db899f21beaa7a48",
+    "url": "/css/app.289203bb.css"
+  },
+  {
+    "revision": "2eeab9bee621a151ff23",
+    "url": "/css/chunk-vendors.c5a6a507.css"
+  },
+  {
+    "revision": "2eebd3156e3d8131e741776982a9bd71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddf5db899f21beaa7a48",
+    "url": "/js/app.48cffabf.js"
+  },
+  {
+    "revision": "2eeab9bee621a151ff23",
+    "url": "/js/chunk-vendors.16fde173.js"
+  },
+  {
+    "revision": "2609b25782180469094291f754d2ca3b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
